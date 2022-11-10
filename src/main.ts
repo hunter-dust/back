c73b8619384d794d939ab929868de3620c4d0c60
Jiyu Jang <apryll7995@gmail.com>
2022-11-10 21:20:59 +0900

    fix main.ts (set the environment variable port)

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(
+    `🟢 ${port} 포트로 서버가 열렸어요! http://localhost:${port}/api`,
+  );
 }
 bootstrap();
